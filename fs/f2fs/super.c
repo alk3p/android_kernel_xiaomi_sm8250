b29b1f2a0cfd77cdb258342f3278e2da0f22c1e5
Park Ju Hyung <qkrwngud825@gmail.com>
2019-08-09 22:10:02 +0900

    f2fs: catch a global wakelock instead of per gc_kthread
    Catching wakelocks per gc_kthread is unnecessary and complicates
    create/destroy logic.
    
    Hold a mutex and count GC users instead to catch a global wakelock.
    Mutex has been used as gc_wakelock cannot be atomic.
    
    This also fixes stack-use-after-scope causing panics on reading
    /sys/kernel/debug/wakeup_sources, first brought to attention by ab123321.
    
    Also, use proper initialize and destroy methods.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: I97aad3ccd22c1e2fa63b38c8a58410d143af7a49

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -4420,6 +4420,8 @@ static int __init init_f2fs_fs(void)
 	err = f2fs_create_casefold_cache();
 	if (err)
 		goto free_compress_cache;
+	f2fs_init_rapid_gc();
+
 	return 0;
 free_compress_cache:
 	f2fs_destroy_compress_cache();
@@ -4458,6 +4460,7 @@ fail:
 
 static void __exit exit_f2fs_fs(void)
 {
+	f2fs_destroy_rapid_gc();
 	f2fs_destroy_casefold_cache();
 	f2fs_destroy_compress_cache();
 	f2fs_destroy_compress_mempool();
