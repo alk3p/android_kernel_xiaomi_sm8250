68389be2614380e24c5e6dae4e504145f93962a6
Pankaj Singh <pansin@codeaurora.org>
2020-02-27 10:14:00 +0530

    qcacld-3.0: Disable all roam scan in case p2p connection is present
    Glitches seen after the STA disconnection because of the roam scan
    triggered on a heartbeat failure ( beacon miss ).STA does a partial
    scan followed by full scan causing the DUT to go off-channel from
    the STA and P2P home channel, causing the music BLIP.
    
    Fix is to disable roaming irrespective of data traffic present
    or not on P2P interface and enabled again once P2P is disconnected.
    
    Change-Id: I5ddf946adbfbe99d5b20c23c46b2b4ac0bfac47f
    CRs-Fixed: 2632939

--- core/sme/src/csr/csr_api_roam.c
+++ core/sme/src/csr/csr_api_roam.c
@@ -64,6 +64,7 @@
 #include "wlan_qct_sys.h"
 #include "wlan_blm_api.h"
 #include "wlan_policy_mgr_i.h"
+#include "wlan_p2p_cfg_api.h"
 
 #define RSN_AUTH_KEY_MGMT_SAE           WLAN_RSN_SEL(WLAN_AKM_SAE)
 #define MAX_PWR_FCC_CHAN_12 8
@@ -20428,6 +20429,16 @@ csr_roam_offload_scan(struct mac_context *mac_ctx, uint8_t session_id,
 		}
 	}
 
+	if (cfg_p2p_is_roam_config_disabled(mac_ctx->psoc) &&
+	    (command == ROAM_SCAN_OFFLOAD_START ||
+	     command == ROAM_SCAN_OFFLOAD_UPDATE_CFG) &&
+	    (policy_mgr_mode_specific_connection_count(mac_ctx->psoc,
+						PM_P2P_CLIENT_MODE, NULL) ||
+	    policy_mgr_mode_specific_connection_count(mac_ctx->psoc,
+						PM_P2P_GO_MODE, NULL))) {
+		sme_info("roaming not supported for active p2p connection");
+		return QDF_STATUS_E_FAILURE;
+	}
 	/*
 	 * The Dynamic Config Items Update may happen even if the state is in
 	 * INIT. It is important to ensure that the command is passed down to
