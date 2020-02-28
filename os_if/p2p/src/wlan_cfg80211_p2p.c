bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: Reduce excessive logging in P2P module
Currently there are lot of duplicate logging. Identify and
reduce those logs that are not absolutely necessary.

Change-Id: I649ce3bec8c3c8542fe9edd08907da9ab01fa948
CRs-Fixed: 2617504

os_if/p2p/src/wlan_cfg80211_p2p.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -83,8 +83,8 @@ static void wlan_p2p_rx_callback(void *user_data,
 		goto fail;
 	}
 
-	osif_debug("Indicate frame over nl80211, vdev id:%d, idx:%d",
-		   rx_frame->vdev_id, wdev->netdev->ifindex);
+	osif_debug("Indicate frame over nl80211, idx:%d",
+		   wdev->netdev->ifindex);
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 18, 0))
 	cfg80211_rx_mgmt(wdev, rx_frame->rx_freq, rx_frame->rx_rssi * 100,
@@ -120,10 +120,6 @@ static void wlan_p2p_action_tx_cnf_callback(void *user_data,
 	struct vdev_osif_priv *osif_priv;
 	struct wireless_dev *wdev;
 	bool is_success;
-
-	osif_debug("user data:%pK, action cookie:%llx, buf:%pK, len:%d, tx status:%d",
-		   user_data, tx_cnf->action_cookie, tx_cnf->buf,
-		   tx_cnf->buf_len, tx_cnf->status);
 
 	psoc = user_data;
 	if (!psoc) {

alk3p/android_kernel_xiaomi_sm8250

os_if/p2p/src/wlan_cfg80211_p2p.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -91,8 +91,8 @@ static void wlan_p2p_rx_callback(void *user_data,
 		freq = ieee80211_channel_to_frequency(
 			rx_frame->rx_chan, NL80211_BAND_5GHZ);
 
-	cfg80211_debug("Indicate frame over nl80211, vdev id:%d, idx:%d",
-		   rx_frame->vdev_id, wdev->netdev->ifindex);
+	cfg80211_debug("Indicate frame over nl80211, idx:%d",
+			wdev->netdev->ifindex);
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 18, 0))
 	cfg80211_rx_mgmt(wdev, freq, rx_frame->rx_rssi * 100,
@@ -128,10 +128,6 @@ static void wlan_p2p_action_tx_cnf_callback(void *user_data,
 	struct vdev_osif_priv *osif_priv;
 	struct wireless_dev *wdev;
 	bool is_success;
-
-	cfg80211_debug("user data:%pK, action cookie:%llx, buf:%pK, len:%d, tx status:%d",
-		user_data, tx_cnf->action_cookie, tx_cnf->buf,
-		tx_cnf->buf_len, tx_cnf->status);
 
 	psoc = user_data;
 	if (!psoc) {