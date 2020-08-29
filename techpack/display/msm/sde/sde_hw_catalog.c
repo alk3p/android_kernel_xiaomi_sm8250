345c92bbe73e36ab4c43c1d90e0f271dae5c09af
LibXZR <xzr467706992@163.com>
2020-08-29 15:15:11 +0800

    techpack/display: sde: Rollback changes of true inline rotation v2
    This reverts the following commits:
    
    76438f9 ("disp: msm: sde: new formats added for true inline rotation v2")
    b254f1e ("disp: msm: sde: pre-downscale support for inline rotation v2")
    4910a9b ("disp: msm: sde: remove inline prefill properties")
    288b4e8 ("disp: msm: sde: determine rotation capabilities from version")
    70ef95b ("disp: msm: sde: fix min pre-downscale check")
    2fe3ae5 ("disp: msm: sde: use helper to determine plane pre-downscale cap")
    d1afe15 ("disp: msm: sde: fix max downscale check for legacy HW")
    40212eb ("disp: msm: sde: expose true inline rotation version capability")
    
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- techpack/display/msm/sde/sde_hw_catalog.c
+++ techpack/display/msm/sde/sde_hw_catalog.c
@@ -75,12 +75,9 @@
 #define MAX_DOWNSCALE_RATIO		4
 #define SSPP_UNITY_SCALE		1
 
-#define MAX_DOWNSCALE_RATIO_INROT_NOPD_RT_NUMERATOR	11
-#define MAX_DOWNSCALE_RATIO_INROT_NOPD_RT_DENOMINATOR	5
-#define MAX_DOWNSCALE_RATIO_INROT_PD_RT_NUMERATOR	4
-#define MAX_DOWNSCALE_RATIO_INROT_PD_RT_DENOMINATOR	1
-#define MAX_DOWNSCALE_RATIO_INROT_NRT_DEFAULT		4
-
+#define MAX_DOWNSCALE_RATIO_INLINE_ROT_RT_NUMERATOR	11
+#define MAX_DOWNSCALE_RATIO_INLINE_ROT_RT_DENOMINATOR	5
+#define MAX_DOWNSCALE_RATIO_INLINE_ROT_NRT_DEFAULT	4
 #define MAX_PRE_ROT_HEIGHT_INLINE_ROT_DEFAULT	1088
 
 #define MAX_HORZ_DECIMATION		4
@@ -1207,33 +1204,24 @@ static void _sde_sspp_setup_vig(struct sde_mdss_cfg *sde_cfg,
 
 	sblk->format_list = sde_cfg->vig_formats;
 	sblk->virt_format_list = sde_cfg->virt_vig_formats;
-	if (sde_cfg->true_inline_rot_rev > 0) {
-		set_bit(SDE_SSPP_TRUE_INLINE_ROT, &sspp->features);
+	if (IS_SDE_INLINE_ROT_REV_100(sde_cfg->true_inline_rot_rev)) {
+		set_bit(SDE_SSPP_TRUE_INLINE_ROT_V1, &sspp->features);
 		sblk->in_rot_format_list = sde_cfg->inline_rot_formats;
+		sblk->in_rot_maxdwnscale_rt_num =
+			sde_cfg->true_inline_dwnscale_rt_num;
+		sblk->in_rot_maxdwnscale_rt_denom =
+			sde_cfg->true_inline_dwnscale_rt_denom;
+		sblk->in_rot_maxdwnscale_nrt =
+			sde_cfg->true_inline_dwnscale_nrt;
 		sblk->in_rot_maxheight = sde_cfg->inline_linewidth ?
 				sde_cfg->inline_linewidth :
 			MAX_PRE_ROT_HEIGHT_INLINE_ROT_DEFAULT;
-	}
-
-	if (IS_SDE_INLINE_ROT_REV_200(sde_cfg->true_inline_rot_rev)) {
-		set_bit(SDE_SSPP_PREDOWNSCALE, &sspp->features);
-		sblk->in_rot_maxdwnscale_rt_num =
-			MAX_DOWNSCALE_RATIO_INROT_PD_RT_NUMERATOR;
-		sblk->in_rot_maxdwnscale_rt_denom =
-			MAX_DOWNSCALE_RATIO_INROT_PD_RT_DENOMINATOR;
-		sblk->in_rot_maxdwnscale_nrt =
-			MAX_DOWNSCALE_RATIO_INROT_NRT_DEFAULT;
-		sblk->in_rot_minpredwnscale_num =
-			MAX_DOWNSCALE_RATIO_INROT_NOPD_RT_NUMERATOR;
-		sblk->in_rot_minpredwnscale_denom =
-			MAX_DOWNSCALE_RATIO_INROT_NOPD_RT_DENOMINATOR;
-	} else if (IS_SDE_INLINE_ROT_REV_100(sde_cfg->true_inline_rot_rev)) {
-		sblk->in_rot_maxdwnscale_rt_num =
-			MAX_DOWNSCALE_RATIO_INROT_NOPD_RT_NUMERATOR;
-		sblk->in_rot_maxdwnscale_rt_denom =
-			MAX_DOWNSCALE_RATIO_INROT_NOPD_RT_DENOMINATOR;
-		sblk->in_rot_maxdwnscale_nrt =
-			MAX_DOWNSCALE_RATIO_INROT_NRT_DEFAULT;
+		sblk->in_rot_prefill_fudge_lines =
+			sde_cfg->true_inline_prefill_fudge_lines;
+		sblk->in_rot_prefill_lines_nv12 =
+			sde_cfg->true_inline_prefill_lines_nv12;
+		sblk->in_rot_prefill_lines =
+			sde_cfg->true_inline_prefill_lines;
 	}
 
 	if (sde_cfg->sc_cfg.has_sys_cache) {
@@ -2256,7 +2244,8 @@ static int sde_rot_parse_dt(struct device_node *np,
 	if (rc) {
 		/*
 		 * This is not a fatal error, system cache can be disabled
-		 * in device tree.
+		 * in device tree, anyways recommendation is to have it
+		 * enabled, so print an error but don't fail
 		 */
 		SDE_DEBUG("sys cache will be disabled rc:%d\n", rc);
 		rc = 0;
@@ -4011,86 +4000,58 @@ static int sde_hardware_format_caps(struct sde_mdss_cfg *sde_cfg,
 	uint32_t virt_vig_list_size, in_rot_list_size = 0;
 	uint32_t cursor_list_size = 0;
 	uint32_t index = 0;
-	const struct sde_format_extended *inline_fmt_tbl;
 
-	/* cursor input formats */
+
 	if (sde_cfg->has_cursor) {
 		cursor_list_size = ARRAY_SIZE(cursor_formats);
 		sde_cfg->cursor_formats = kcalloc(cursor_list_size,
 			sizeof(struct sde_format_extended), GFP_KERNEL);
 		if (!sde_cfg->cursor_formats) {
 			rc = -ENOMEM;
-			goto out;
+			goto end;
 		}
 		index = sde_copy_formats(sde_cfg->cursor_formats,
 			cursor_list_size, 0, cursor_formats,
 			ARRAY_SIZE(cursor_formats));
 	}
 
-	/* DMA pipe input formats */
 	dma_list_size = ARRAY_SIZE(plane_formats);
+	vig_list_size = ARRAY_SIZE(plane_formats_vig);
+	if (sde_cfg->has_vig_p010)
+		vig_list_size += ARRAY_SIZE(p010_ubwc_formats);
+	virt_vig_list_size = ARRAY_SIZE(plane_formats);
+	wb2_list_size = ARRAY_SIZE(wb2_formats);
+
+	if (IS_SDE_INLINE_ROT_REV_100(sde_cfg->true_inline_rot_rev))
+		in_rot_list_size = ARRAY_SIZE(true_inline_rot_v1_fmts);
 
 	sde_cfg->dma_formats = kcalloc(dma_list_size,
 		sizeof(struct sde_format_extended), GFP_KERNEL);
 	if (!sde_cfg->dma_formats) {
 		rc = -ENOMEM;
-		goto free_cursor;
+		goto end;
 	}
-
-	index = sde_copy_formats(sde_cfg->dma_formats, dma_list_size,
-			0, plane_formats, ARRAY_SIZE(plane_formats));
-
-	/* ViG pipe input formats */
-	vig_list_size = ARRAY_SIZE(plane_formats_vig);
-	if (sde_cfg->has_vig_p010)
-		vig_list_size += ARRAY_SIZE(p010_ubwc_formats);
 
 	sde_cfg->vig_formats = kcalloc(vig_list_size,
 		sizeof(struct sde_format_extended), GFP_KERNEL);
 	if (!sde_cfg->vig_formats) {
 		rc = -ENOMEM;
-		goto free_dma;
+		goto end;
 	}
 
-	index = sde_copy_formats(sde_cfg->vig_formats, vig_list_size,
-			0, plane_formats_vig, ARRAY_SIZE(plane_formats_vig));
-	if (sde_cfg->has_vig_p010)
-		index += sde_copy_formats(sde_cfg->vig_formats,
-			vig_list_size, index, p010_ubwc_formats,
-			ARRAY_SIZE(p010_ubwc_formats));
-
-	/* Virtual ViG pipe input formats (all virt pipes use DMA formats) */
-	virt_vig_list_size = ARRAY_SIZE(plane_formats);
 	sde_cfg->virt_vig_formats = kcalloc(virt_vig_list_size,
 		sizeof(struct sde_format_extended), GFP_KERNEL);
 	if (!sde_cfg->virt_vig_formats) {
 		rc = -ENOMEM;
-		goto free_vig;
+		goto end;
 	}
 
-	index = sde_copy_formats(sde_cfg->virt_vig_formats, virt_vig_list_size,
-			0, plane_formats, ARRAY_SIZE(plane_formats));
-
-	/* WB output formats */
-	wb2_list_size = ARRAY_SIZE(wb2_formats);
 	sde_cfg->wb_formats = kcalloc(wb2_list_size,
 		sizeof(struct sde_format_extended), GFP_KERNEL);
 	if (!sde_cfg->wb_formats) {
 		SDE_ERROR("failed to allocate wb format list\n");
 		rc = -ENOMEM;
-		goto free_virt;
-	}
-
-	index = sde_copy_formats(sde_cfg->wb_formats, wb2_list_size,
-			 0, wb2_formats, ARRAY_SIZE(wb2_formats));
-
-	/* Rotation enabled input formats */
-	if (IS_SDE_INLINE_ROT_REV_100(sde_cfg->true_inline_rot_rev)) {
-		inline_fmt_tbl = true_inline_rot_v1_fmts;
-		in_rot_list_size = ARRAY_SIZE(true_inline_rot_v1_fmts);
-	} else if (IS_SDE_INLINE_ROT_REV_200(sde_cfg->true_inline_rot_rev)) {
-		inline_fmt_tbl = true_inline_rot_v2_fmts;
-		in_rot_list_size = ARRAY_SIZE(true_inline_rot_v2_fmts);
+		goto end;
 	}
 
 	if (in_rot_list_size) {
@@ -4099,27 +4060,30 @@ static int sde_hardware_format_caps(struct sde_mdss_cfg *sde_cfg,
 		if (!sde_cfg->inline_rot_formats) {
 			SDE_ERROR("failed to alloc inline rot format list\n");
 			rc = -ENOMEM;
-			goto free_wb;
+			goto end;
 		}
-
-		index = sde_copy_formats(sde_cfg->inline_rot_formats,
-			in_rot_list_size, 0, inline_fmt_tbl, in_rot_list_size);
 	}
 
-	return 0;
+	index = sde_copy_formats(sde_cfg->dma_formats, dma_list_size,
+		0, plane_formats, ARRAY_SIZE(plane_formats));
 
-free_wb:
-	kfree(sde_cfg->wb_formats);
-free_virt:
-	kfree(sde_cfg->virt_vig_formats);
-free_vig:
-	kfree(sde_cfg->vig_formats);
-free_dma:
-	kfree(sde_cfg->dma_formats);
-free_cursor:
-	if (sde_cfg->has_cursor)
-		kfree(sde_cfg->cursor_formats);
-out:
+	index = sde_copy_formats(sde_cfg->vig_formats, vig_list_size,
+		0, plane_formats_vig, ARRAY_SIZE(plane_formats_vig));
+	if (sde_cfg->has_vig_p010)
+		index += sde_copy_formats(sde_cfg->vig_formats,
+			vig_list_size, index, p010_ubwc_formats,
+			ARRAY_SIZE(p010_ubwc_formats));
+
+	index = sde_copy_formats(sde_cfg->virt_vig_formats, virt_vig_list_size,
+		0, plane_formats, ARRAY_SIZE(plane_formats));
+
+	index = sde_copy_formats(sde_cfg->wb_formats, wb2_list_size,
+		0, wb2_formats, ARRAY_SIZE(wb2_formats));
+	if (in_rot_list_size)
+		index = sde_copy_formats(sde_cfg->inline_rot_formats,
+			in_rot_list_size, 0, true_inline_rot_v1_fmts,
+			ARRAY_SIZE(true_inline_rot_v1_fmts));
+end:
 	return rc;
 }
 
@@ -4279,6 +4243,15 @@ static int _sde_hardware_pre_caps(struct sde_mdss_cfg *sde_cfg, uint32_t hw_rev)
 		set_bit(SDE_MDP_DHDR_MEMPOOL, &sde_cfg->mdp[0].features);
 		sde_cfg->has_vig_p010 = true;
 		sde_cfg->true_inline_rot_rev = SDE_INLINE_ROT_VERSION_1_0_0;
+		sde_cfg->true_inline_dwnscale_rt_num =
+			MAX_DOWNSCALE_RATIO_INLINE_ROT_RT_NUMERATOR;
+		sde_cfg->true_inline_dwnscale_rt_denom =
+			MAX_DOWNSCALE_RATIO_INLINE_ROT_RT_DENOMINATOR;
+		sde_cfg->true_inline_dwnscale_nrt =
+			MAX_DOWNSCALE_RATIO_INLINE_ROT_NRT_DEFAULT;
+		sde_cfg->true_inline_prefill_fudge_lines = 2;
+		sde_cfg->true_inline_prefill_lines_nv12 = 32;
+		sde_cfg->true_inline_prefill_lines = 48;
 		sde_cfg->uidle_cfg.uidle_rev = SDE_UIDLE_VERSION_1_0_0;
 	} else if (IS_SAIPAN_TARGET(hw_rev)) {
 		sde_cfg->has_cwb_support = true;
@@ -4301,6 +4274,15 @@ static int _sde_hardware_pre_caps(struct sde_mdss_cfg *sde_cfg, uint32_t hw_rev)
 		set_bit(SDE_MDP_DHDR_MEMPOOL, &sde_cfg->mdp[0].features);
 		sde_cfg->has_vig_p010 = true;
 		sde_cfg->true_inline_rot_rev = SDE_INLINE_ROT_VERSION_1_0_0;
+		sde_cfg->true_inline_dwnscale_rt_num =
+			MAX_DOWNSCALE_RATIO_INLINE_ROT_RT_NUMERATOR;
+		sde_cfg->true_inline_dwnscale_rt_denom =
+			MAX_DOWNSCALE_RATIO_INLINE_ROT_RT_DENOMINATOR;
+		sde_cfg->true_inline_dwnscale_nrt =
+			MAX_DOWNSCALE_RATIO_INLINE_ROT_NRT_DEFAULT;
+		sde_cfg->true_inline_prefill_fudge_lines = 2;
+		sde_cfg->true_inline_prefill_lines_nv12 = 32;
+		sde_cfg->true_inline_prefill_lines = 48;
 		sde_cfg->update_tcsr_disp_glitch = true;
 	} else if (IS_SDMTRINKET_TARGET(hw_rev)) {
 		sde_cfg->has_cwb_support = true;
