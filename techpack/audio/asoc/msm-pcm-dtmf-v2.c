230f12aaae51c09db2d42a8a9e2edc12a412f203
alk3pInjection <webmaster@raspii.tech>
2021-01-05 14:45:25 +0800

    techpack: audio: minor clean-up
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- techpack/audio/asoc/msm-pcm-dtmf-v2.c
+++ techpack/audio/asoc/msm-pcm-dtmf-v2.c
@@ -96,7 +96,7 @@ static int msm_dtmf_rx_generate_put(struct snd_kcontrol *kcontrol,
 	int64_t duration = ucontrol->value.integer.value[2];
 	uint16_t gain = ucontrol->value.integer.value[3];
 
-	pr_debug("%s: low_freq=%d high_freq=%d duration=%lld gain=%d\n",
+	pr_debug("%s: low_freq=%d high_freq=%d duration=%d gain=%d\n",
 		 __func__, low_freq, high_freq, (int)duration, gain);
 
 	if (duration == DTMF_MAX_DURATION)
