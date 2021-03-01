a54431d199838814be99d4c2f8abe8c01e78e072
dreamisbaka <jolinux.g@gmail.com>
2021-03-01 18:51:33 +0800

    input: touchscreen: nt36xxx: Affine IRQ to the big CPU cluster
    Touch latency is pretty important; let's service the touch IRQ and its
    associated kthread on the big CPU cluster to improve touch
    responsiveness.
    
    Signed-off-by: dreamisbaka <jolinux.g@gmail.com>

--- drivers/input/touchscreen/nt36672c/nt36xxx.c
+++ drivers/input/touchscreen/nt36672c/nt36xxx.c
@@ -2545,7 +2545,7 @@ static int32_t nvt_ts_probe(struct platform_device *pdev)
 		NVT_LOG("int_trigger_type=%d\n", ts->int_trigger_type);
 		ts->irq_enabled = true;
 		ret = request_threaded_irq(ts->client->irq, NULL, nvt_ts_work_func,
-				ts->int_trigger_type | IRQF_ONESHOT, NVT_SPI_NAME, ts);
+				ts->int_trigger_type | IRQF_ONESHOT | IRQF_PERF_AFFINE, NVT_SPI_NAME, ts);
 		if (ret != 0) {
 			NVT_ERR("request irq failed. ret=%d\n", ret);
 			goto err_int_request_failed;
